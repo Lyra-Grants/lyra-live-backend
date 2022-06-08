4917983b46967c9604218661f6e3f434312659ce
burtrico <burtrico@gmail.com>
2022-06-07 22:11:19 -0500

    added dependencies and test files

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "optimarket",
+  password: "lyra-live",
   host: "localhost",
   port: 5432,
-  database: "collection"
+  database: "trader"
 });
 
 module.exports = pool;
