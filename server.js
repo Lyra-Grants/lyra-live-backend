7173b6dbabe81ea13fba95c17130e917b058493d
burtrico <burtrico@gmail.com>
2022-06-16 22:25:49 -0500

    added User model and controller

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
+// establish an express app
 const express = require('express')
 const app = express()
 
+// allow requests from outside resources like postman, or your frontend if you choose to build that out
 const cors = require('cors')
 app.use(cors())
 
+// app will serve and receive data in a JSON format
 app.use(express.json())
 
+// the messenger between our app and our database
 const mongoose = require('mongoose')
+
+// allow us to hide our connection secret in the process.env object
 require('dotenv').config()
 
+// establish connection & give yourself a message so you know when its complete
 const source = process.env.ATLAS_CONNECTION
 
 mongoose.connect(source)
-
 const connection = mongoose.connection
 connection.once('open', () => {
-  console.log("DB connected.");
+    console.log("DB connected.");
 })
 
-const PORT = process.env.PORT || 6000
+const userRoutes = require('./controllers/user.controller')
+app.use('/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log(`Successfully served on port: ${PORT}.`);
 })
