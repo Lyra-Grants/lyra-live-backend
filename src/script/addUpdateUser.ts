f18490b203d4460f63060b6345f8f3ff6b40f8ab
burtrico <burtrico@gmail.com>
2022-07-30 23:23:46 -0500

    updated addUpdateUser with .findOne and ,findOneAndUpdate

--- src/script/addUpdateUser.ts
+++ src/script/addUpdateUser.ts
@@ -10,80 +10,44 @@ import server from '../../server'
 const accountsArray: string[] = ['0x90C6577Fb57edF1921ae3F7F45dF7A31e46b9155', '0x23c5c19d2ad460b7cd1ea5d6a2274a3c53733238']
 const DB_URL = 'http://localhost:4000'
 
-const getFetchUser = (address: string) => {
-    const getObj = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user: {
-                account: address
-            }
-        })
-    }
-    let answer: any
-    fetch(`${DB_URL}/`, getObj)
-    .then((result: any) => answer = result)
-    return answer
-}
-
-const res = await fetch('https://nodejs.org/api/documentation.json');
-if (res.ok) {
-  const data = await res.json();
-  console.log(data);
-}
-
-const putFetchUser = (address: string, currentPnl: number) => {
-    const putObj = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user: {
-                account: address,
-                // ens,
-                // avatar,
-                // trades_count,
-                // duration,
-                // favorite_asset,
-                pnl: currentPnl,
-                // positions,
-            }
-        })
-    }
-    let answer: any
-    fetch(`${DB_URL}/update/${address}`, putObj)
-    .then((result: any) => answer = result)
-    return answer
-}
-
 const addUpdateUser = async (accounts: string[]) => {
     await server().then(async (mongoose) => {
         try {
+
             for (let i = 0; i < accounts.length; i++) {
                 const userPositions = await getLyraPositions(accounts[i]);
         
-                let pnl: number = 0;
+                let currentPnl: number = 0;
         
                 userPositions.map((position: IPosition) => {
-                    if(position.realizedPnl) pnl = pnl + position.realizedPnl
+                    if(position.realizedPnl) currentPnl = currentPnl + position.realizedPnl
                 });
-                console.log("pnl =", pnl)
+                console.log("current pnl =", currentPnl)
         
-                const userExists = await getFetchUser(accounts[i]);
-
+                const userExists = await User.findOne({account: accounts[i]});
 
                 console.log("userExists =", userExists)
 
 
                 if(!userExists.success) {
                     // Need to replace the status 400 below to match the error message from not finding the user in the DB
-                    if(userExists.message === `status 400`) await addUser(accounts[i], pnl)
+                    if(userExists.message === `status 400`) {
+                        const newUser = new User({
+                            // _id,
+                            account: accounts[i],
+                            // ens,
+                            // avatar,
+                            // trades_count,
+                            // duration,
+                            // favorite_asset,
+                            pnl: currentPnl,
+                            // positions,
+                        });
+                        await newUser.save();
+                    }
                     else console.log(userExists.message)
                 }
-                else if (userExists.success) await putFetchUser(accounts[i], pnl);
+                else if (userExists.success) await User.findOneAndUpdate({account: accounts[i], pnl: currentPnl});
             }
 
         } finally {
