e3e0762b522f296cd81b631ab8791ece3ebfc765
burtrico <burtrico@gmail.com>
2022-08-22 22:10:32 -0500

    updated seed.ts to show a list of addresses

---
+++ seed.ts
@@ -0,0 +1,10 @@
+import addUpdateUser from "./src/script/addUpdateUser";
+
+const data = [
+    '0x90C6577Fb57edF1921ae3F7F45dF7A31e46b9155', 
+    '0x23c5c19d2ad460b7cd1ea5d6a2274a3c53733238', 
+    '0x6c51fa14f293814bc1e30ec186a7c4896ed359f1'
+]
+
+addUpdateUser(data);
+
